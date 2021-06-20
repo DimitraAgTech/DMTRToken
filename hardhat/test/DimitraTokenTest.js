813ef9751edee30b0f5d7672aa6c485e45bc31ec
peter <peterteach@gmail.com>
2021-06-20 08:24:58 -0700

    Added Token Minting Tests and fixed/cleaned other tests

--- hardhat/test/DimitraTokenTest.js
+++ hardhat/test/DimitraTokenTest.js
@@ -2,47 +2,50 @@ const { expect } = require("chai");
 const { parseEther, formatEther, formatUnits, parseUnits, formatBytes32String, keccak256, id } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
 
-  let token;
-  let dimitraToken;
-  let owner;
-  let account1;
-  let account2;
-  let accounts;
+let token;
+let dimitraToken;
+let owner;
+let account1;
+let account2;
+let accounts;
 
-  // `beforeEach` will run before each test, re-deploying the contract every
-  // time. It receives a callback, which can be async.
-  beforeEach(async function () {
-    token = await ethers.getContractFactory("DimitraToken");
-    [owner, account1, account2, ...accounts] = await ethers.getSigners();
-    dimitraToken = await token.deploy();
-  });
+// `beforeEach` will run before each test, re-deploying the contract every
+// time. It receives a callback, which can be async.
+beforeEach(async function () {
+  contractFactory = await ethers.getContractFactory("DimitraToken");
+  [owner, account1, account2, ...accounts] = await ethers.getSigners();
+  dimitraToken = await contractFactory.deploy();
+});
 
-describe("Token contract Deployment", function() {
-  it("Deployment should have name 'Dimitra Token', symbol 'DMTR', decimals 18,.", async function() {
+describe("Token Deployment Tests", function() {
+
+  it("Deployment should have name 'Dimitra Token', symbol 'DMTR', decimals 18", async function() {
     expect(await dimitraToken.name()).to.equal('Dimitra Token');
     expect(await dimitraToken.symbol()).to.equal('DMTR');
     expect(await dimitraToken.decimals()).to.equal(18);
   });
 
-  it("Deployment should assign the mintable tokens to the owner", async function() {
-    const ownerBalance = await dimitraToken.balanceOf(owner.address);
-    console.log(formatEther(ownerBalance));
-    expect(await dimitraToken.totalSupply()).to.equal(ownerBalance);
+  it("Deployment should assign supply of zero", async function() {
+    expect(await dimitraToken.totalSupply()).to.equal(0);
   });
 
-  it("Deployment should assign cap of 1 billion",async function(){
+  it("Deployment should assign cap of 1 billion", async function(){
     expect(formatEther(await dimitraToken.cap())).to.equal(formatUnits( parseUnits("1000000000", 18)));
   })
+
+  it("Deployment should assign owner balance of zero", async function() {
+    expect(await dimitraToken.balanceOf(owner.address)).to.equal(0);
+  });
 });
 
 describe("Token Role Tests", function() {
   it("Check and return various roles", async function() {
     console.log("owner.address is ", owner.address);
-    console.log("MINTER_ROLE should be owner ", (await dimitraToken.hasRole(id("MINTER_ROLE"), owner.address)));
-    console.log("PAUSER_ROLE should be owner ", (await dimitraToken.hasRole(id("PAUSER_ROLE"), owner.address)));
-    console.log("BURNER_ROLE should be owner ", (await dimitraToken.hasRole(id("BURNER_ROLE"), owner.address)));
-    console.log("ADMIN_ROLE should be owner ", (await dimitraToken.hasRole(id("ADMIN_ROLE"), owner.address)));
-    console.log("ADMIN_ROLE2 should be owner ", (await dimitraToken.hasRole(id("ADMIN_ROLE2"), owner.address)));
+    console.log("owner should be MINTER_ROLE ", (await dimitraToken.hasRole(id("MINTER_ROLE"), owner.address)));
+    console.log("owner should be PAUSER_ROLE ", (await dimitraToken.hasRole(id("PAUSER_ROLE"), owner.address)));
+    console.log("owner should be BURNER_ROLE ", (await dimitraToken.hasRole(id("BURNER_ROLE"), owner.address)));
+    console.log("owner should be ADMIN_ROLE  ", (await dimitraToken.hasRole(id("ADMIN_ROLE"), owner.address)));
+    console.log("owner should be ADMIN_ROLE2 ", (await dimitraToken.hasRole(id("ADMIN_ROLE2"), owner.address)));
   })
 
   it("Grant roles test", async function() {
@@ -66,14 +69,39 @@ describe("Token Role Tests", function() {
       if (e.message && e.message === 'Expected error') throw e
       console.log("Granting role from non owner account was prevented");
     }
-  })
+  });
+});
 
-// hasRole(role, account)
-// getRoleMemberCount(role)
-// getRoleMember(role, index)
-// getRoleAdmin(role)
-// grantRole(role, account)
-// revokeRole(role, account)
-// renounceRole(role, account)
+describe("Token Minting Tests", function() {
 
-})
+  it("Non-owner cannot mint tokens", async function() {
+    try {
+      await dimitraToken.connect(account1).mint(account1.address, 10);
+      throw new Error('Expected error');
+    }
+    catch (e) {
+      if (e.message && e.message === 'Expected error') throw e
+      console.log("Minting tokens from non owner account was prevented");
+    }
+  });
+  
+  it("Minted tokens can be assigned by owner to owner address", async function() {
+    const mintAmount = '1000000000000000000';
+    const balanceBeforeMinting = await dimitraToken.balanceOf(owner.address);
+    console.log("balanceBeforeMinting", formatEther(balanceBeforeMinting));
+    await dimitraToken.connect(owner).mint(owner.address, mintAmount);
+    const balanceAfterMinting = await dimitraToken.balanceOf(owner.address);
+    console.log("balanceAfterMinting", formatEther(balanceAfterMinting));
+    expect(balanceAfterMinting).to.equal(balanceBeforeMinting + mintAmount);
+  });
+
+  it("Minted tokens can be assigned by owner to another address", async function() {
+    const mintAmount = '1000000000000000000';
+    const balanceBeforeMinting = await dimitraToken.balanceOf(account1.address);
+    console.log("balanceBeforeMinting", formatEther(balanceBeforeMinting));
+    await dimitraToken.connect(owner).mint(account1.address, mintAmount);
+    const balanceAfterMinting = await dimitraToken.balanceOf(account1.address);
+    console.log("balanceAfterMinting", formatEther(balanceAfterMinting));
+    expect(balanceAfterMinting).to.equal(balanceBeforeMinting + mintAmount);
+  });
+});
